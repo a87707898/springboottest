1800668e2b12ff2f437d7c37bba57aa6e1e0e590
ZhangJie <zhang___jie@126.com>
2017-07-11 19:59:56 +0800

    增加一个调通的resource

--- src/main/java/com/huawei/zj/verify/resource/UserController.java
+++ src/main/java/com/huawei/zj/verify/resource/UserController.java
@@ -13,4 +13,11 @@ public class UserController
     {
         return "Welcome," + username;
     }
+
+    //http://localhost:31025/groups/zhangjie
+    @RequestMapping(value = "/groups/{groupname}", method = RequestMethod.GET)
+    public String getUser(@PathVariable String groupname)
+    {
+        return "Welcome," + groupname;
+    }
 }
